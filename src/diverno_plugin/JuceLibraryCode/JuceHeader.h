2a73823b0c6293ac5a6200a53f3e28ec8109785f
sam-trolland <sam.trolland@monash.edu>
2021-07-14 17:13:50 +1000

    Revert "MarkovChain.cpp" - This reverts commit 197e06751429e8f13bbe273301b26dc6a59432dc. - Remove Sam's Multi Order Markov Model match code

--- src/diverno_plugin/JuceLibraryCode/JuceHeader.h
+++ src/diverno_plugin/JuceLibraryCode/JuceHeader.h
@@ -50,7 +50,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "dinverno_plugin";
     const char* const  companyName    = "";
-    const char* const  versionString  = "1.1.2";
-    const int          versionNumber  = 0x10102;
+    const char* const  versionString  = "1.0.0";
+    const int          versionNumber  = 0x10000;
 }
 #endif
